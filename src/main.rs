637c64bc409be2b8361f7768dfc015ba27842c2d
Martin <wienc.martin@gmail.com>
2022-12-31 03:39:24 -0500

    added delete file, create/delete dir impl

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use axum::body::boxed;
 use axum::body::Body;
 use axum::http::{header, HeaderValue, Method, Request, Uri};
+use axum::routing::delete;
 use axum::{
     body::Bytes,
     extract::{BodyStream, Path},
@@ -11,18 +12,19 @@ use axum::{
 };
 use futures::{Stream, TryStreamExt};
 
+use chrono::{DateTime, Utc};
 use serde::Serialize;
 use std::io::Error;
 use std::os::unix::fs::MetadataExt;
 use std::sync::Arc;
-use std::time::{Duration, SystemTime};
+use std::time::Duration;
 use std::{io, net::SocketAddr};
 use tokio::fs::File;
 use tokio::fs::{metadata, read_dir};
 use tokio::io::BufWriter;
 use tokio_util::io::StreamReader;
 use tower::{ServiceBuilder, ServiceExt};
-use tower_http::cors::{CorsLayer};
+use tower_http::cors::CorsLayer;
 use tower_http::services::ServeFile;
 use tower_http::{
     timeout::TimeoutLayer,
@@ -37,7 +39,7 @@ struct FileInfo {
     name: String,
     is_directory: bool,
     size: u64,
-    modified_at: u64,
+    modified_at: String,
 }
 
 const UPLOADS_DIRECTORY: &str = "uploads";
@@ -73,7 +75,8 @@ async fn main() {
             Method::TRACE,
         ])
         .allow_origin([
-            "http://localhost:5173".parse().unwrap(),
+            "http://localhost:5173".parse::<HeaderValue>().unwrap(),
+            "http://localhost:4173".parse::<HeaderValue>().unwrap(),
         ]);
     let middleware = ServiceBuilder::new()
         .sensitive_request_headers(sensitive_headers.clone())
@@ -98,12 +101,11 @@ async fn main() {
     let bucket_routes = Router::new()
         .route("/", get(get_buckets))
         .route("/:bucket_name/*path", get(get_route))
-        .route("/:bucket_name/*path", post(save_request_body));
+        .route("/:bucket_name/*path", post(save_request_body))
+        .route("/:bucket_name/*path", delete(delete_request));
     let api_routes = Router::new().nest("/buckets", bucket_routes);
 
-    let app = Router::new()
-        .nest("/api", api_routes)
-        .layer(middleware);
+    let app = Router::new().nest("/api", api_routes).layer(middleware);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("listening on {}", addr);
@@ -129,20 +131,41 @@ async fn get_buckets() -> impl IntoResponse {
     (StatusCode::OK, Json(buckets))
 }
 
+async fn delete_request(
+    Path(params): Path<(String, String)>,
+) -> Result<StatusCode, (StatusCode, String)> {
+    let path = format!("{}/{}/{}", &UPLOADS_DIRECTORY, &params.0, &params.1);
+    if let Ok(_) = tokio::fs::read_dir(&path).await {
+        match tokio::fs::remove_dir_all(&path).await {
+            Ok(_) => return Ok(StatusCode::NO_CONTENT),
+            Err(error) => return Err((StatusCode::INTERNAL_SERVER_ERROR, error.to_string())),
+        }
+    }
+    if let Err(error) = tokio::fs::remove_file(path).await {
+        return Err((StatusCode::INTERNAL_SERVER_ERROR, error.to_string()));
+    }
+    Ok(StatusCode::NO_CONTENT)
+}
 // Handler that streams the request body to a file.
 async fn save_request_body(
     Path(params): Path<(String, String)>,
     body: BodyStream,
 ) -> Result<(), (StatusCode, String)> {
     let path = format!("{}/{}/{}", &UPLOADS_DIRECTORY, &params.0, &params.1);
+    if !is_file(&path) {
+        match tokio::fs::create_dir(path).await {
+            Ok(_) => return Ok(()),
+            Err(error) => return Err((StatusCode::INTERNAL_SERVER_ERROR, error.to_string())),
+        }
+    }
     stream_to_file(path.as_str(), body).await
 }
 
 // Save a `Stream` to a file
 async fn stream_to_file<S, E>(path: &str, stream: S) -> Result<(), (StatusCode, String)>
-    where
-        S: Stream<Item=Result<Bytes, E>>,
-        E: Into<BoxError>,
+where
+    S: Stream<Item = Result<Bytes, E>>,
+    E: Into<BoxError>,
 {
     // if !path_is_valid(path) {
     //     return Err((StatusCode::BAD_REQUEST, "Invalid path".to_owned()));
@@ -164,13 +187,12 @@ async fn stream_to_file<S, E>(path: &str, stream: S) -> Result<(), (StatusCode, 
 
         Ok::<_, Error>(())
     }
-        .await
-        .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))
+    .await
+    .map_err(|err| (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()))
 }
 
 async fn get_route(Path(params): Path<(String, String)>, uri: Uri) -> impl IntoResponse {
     let path = format!("{}/{}{}", &UPLOADS_DIRECTORY, params.0, params.1);
-    tracing::debug!("{}", &path);
     let Ok(meta) = metadata(&path).await else {
         return Err((StatusCode::BAD_REQUEST, "Invalid path".to_string()));
     };
@@ -190,17 +212,14 @@ async fn get_dir(path: &String) -> impl IntoResponse {
             break;
         };
         let metadata = entry.metadata().await.unwrap();
+        let modified_at: DateTime<Utc> = metadata.modified().unwrap().into();
         file_infos.push(FileInfo {
             name: String::from(entry.file_name().to_str().unwrap()),
             is_directory: metadata.is_dir(),
             size: metadata.size(),
-            modified_at: metadata
-                .modified()
-                .unwrap()
-                .duration_since(SystemTime::UNIX_EPOCH)
-                .unwrap()
-                .as_secs(),
+            modified_at: modified_at.to_rfc3339(),
         });
+        file_infos.sort_by(|a, b| b.is_directory.cmp(&a.is_directory));
     }
     (StatusCode::OK, Json(file_infos))
 }
@@ -214,6 +233,13 @@ async fn serve_file(path: &str, uri: &Uri) -> impl IntoResponse {
             format!("Something went wrong: {}", err),
         )),
     }
+}
+
+fn is_file(path: &str) -> bool {
+    let path_split = path.split("/");
+    let path = path_split.last().unwrap();
+    let split: Vec<&str> = path.split(".").collect();
+    split.len() > 1
 }
 //
 // fn path_is_valid(path: &str) -> bool {
