07cab21598114394f2bcd9e5988bdee6d269d5c3
Martin <wienc.martin@gmail.com>
2023-06-30 19:54:45 -0400

    impl discord auth

---
+++ migrations/20230630031555_init.sql
@@ -0,0 +1,18 @@
+-- Add migration script here
+CREATE TABLE IF NOT EXISTS users 
+(
+    id          INTEGER PRIMARY KEY NOT NULL,
+    name        TEXT                NOT NULL,
+    email       TEXT                NOT NULL,
+    password_hash TEXT              NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS user_invites
+(
+    id          INTEGER PRIMARY KEY NOT NULL,
+    user_id     INTEGER             NOT NULL,
+    invite_key  TEXT                NOT NULL,
+    email       TEXT                NOT NULL,
+    accepted    BOOLEAN             not null default 0
+);
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use axum::body::boxed;
 use axum::body::Body;
+use axum::extract::Query;
 use axum::http::{header, HeaderValue, Method, Request, Uri};
+use axum::response::Redirect;
 use axum::routing::delete;
+use axum::Extension;
 use axum::{
     body::Bytes,
     extract::{BodyStream, Path},
@@ -10,10 +13,31 @@ use axum::{
     routing::{get, post},
     BoxError, Json, Router,
 };
+use axum_extra::extract::cookie::Cookie;
+use axum_extra::extract::CookieJar;
+use axum_login::axum_sessions::async_session::MemoryStore;
+use axum_login::axum_sessions::extractors::ReadableSession;
+use axum_login::axum_sessions::extractors::WritableSession;
+use axum_login::axum_sessions::SameSite;
+use axum_login::axum_sessions::SessionLayer;
+use axum_login::secrecy::SecretVec;
+use axum_login::AuthLayer;
+use axum_login::AuthUser;
+use axum_login::RequireAuthorizationLayer;
+use axum_login::SqliteStore;
 use futures::{Stream, TryStreamExt};
+use oauth2::TokenResponse;
+use oauth2::{
+    basic::BasicClient, reqwest::async_http_client, AuthUrl, AuthorizationCode, ClientId,
+    ClientSecret, CsrfToken, RedirectUrl, Scope, TokenUrl,
+};
 
 use chrono::{DateTime, Utc};
+use rand::Rng;
+use serde::Deserialize;
 use serde::Serialize;
+use sqlx::SqlitePool;
+use std::env;
 use std::io::Error;
 use std::os::unix::fs::MetadataExt;
 use std::sync::Arc;
@@ -24,6 +48,7 @@ use tokio::fs::{metadata, read_dir};
 use tokio::io::BufWriter;
 use tokio_util::io::StreamReader;
 use tower::{ServiceBuilder, ServiceExt};
+use tower_http::cors::Any;
 use tower_http::cors::CorsLayer;
 use tower_http::services::ServeFile;
 use tower_http::{
@@ -31,6 +56,7 @@ use tower_http::{
     trace::{DefaultMakeSpan, DefaultOnResponse, TraceLayer},
     LatencyUnit, ServiceBuilderExt,
 };
+use tracing::log;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[derive(Serialize)]
@@ -42,16 +68,67 @@ struct FileInfo {
     modified_at: String,
 }
 
+#[derive(Clone)]
+pub struct AppState {
+    pool: SqlitePool,
+}
+
+#[derive(Debug, Default, Clone, sqlx::FromRow)]
+struct User {
+    id: i64,
+    password_hash: String,
+    name: String,
+    email: String,
+}
+
+impl AuthUser<i64> for User {
+    fn get_id(&self) -> i64 {
+        self.id
+    }
+
+    fn get_password_hash(&self) -> SecretVec<u8> {
+        SecretVec::new(self.password_hash.clone().into())
+    }
+}
+
+#[derive(Debug, Default, Clone, sqlx::FromRow)]
+struct UserInvite {
+    id: i64,
+    user_id: i64,
+    invite_key: String,
+    email: String,
+    accepted: bool,
+}
+
+type AuthContext = axum_login::extractors::AuthContext<i64, User, SqliteStore<User>>;
 const UPLOADS_DIRECTORY: &str = "uploads";
 
 #[tokio::main]
 async fn main() {
+    dotenvy::dotenv().ok();
     tracing_subscriber::registry()
         .with(tracing_subscriber::EnvFilter::new(
             std::env::var("RUST_LOG").unwrap_or_else(|_| "mert-bucket-api=debug".into()),
         ))
         .with(tracing_subscriber::fmt::layer())
         .init();
+    let secret = rand::thread_rng().gen::<[u8; 64]>();
+
+    let session_store = MemoryStore::new();
+    let session_layer = SessionLayer::new(session_store, &secret)
+        .with_secure(false)
+        .with_same_site_policy(SameSite::Lax);
+    let pool = SqlitePool::connect(&env::var("DATABASE_URL").expect("Expected DATABASE_URL"))
+        .await
+        .unwrap();
+    sqlx::migrate!()
+        .run(&pool)
+        .await
+        .expect("Error running migrations");
+
+    let user_store = SqliteStore::<User>::new(pool.clone());
+    let auth_layer = AuthLayer::new(user_store, &secret);
+    let oauth_client = build_discord_oauth_client();
 
     let sensitive_headers: Arc<[_]> = vec![header::AUTHORIZATION, header::COOKIE].into();
     let cors = CorsLayer::new()
@@ -74,9 +151,11 @@ async fn main() {
             Method::PATCH,
             Method::TRACE,
         ])
+        .allow_credentials(true)
         .allow_origin([
             "http://localhost:5173".parse::<HeaderValue>().unwrap(),
             "http://localhost:4173".parse::<HeaderValue>().unwrap(),
+            "https://discord.com".parse::<HeaderValue>().unwrap(),
         ]);
     let middleware = ServiceBuilder::new()
         .sensitive_request_headers(sensitive_headers.clone())
@@ -101,11 +180,31 @@ async fn main() {
     let bucket_routes = Router::new()
         .route("/", get(get_buckets))
         .route("/:bucket_name/*path", get(get_route))
-        .route("/:bucket_name/*path", post(save_request_body))
-        .route("/:bucket_name/*path", delete(delete_request));
-    let api_routes = Router::new().nest("/buckets", bucket_routes);
+        .route("/:bucket_name/*path", post(save_request))
+        .route("/:bucket_name/*path", delete(delete_request))
+        .route_layer(RequireAuthorizationLayer::<i64, User>::login());
+    let auth_routes = Router::new()
+        .route("/login", get(login_handler))
+        .route("/discord/callback", get(oauth_callback_handler))
+        .layer(Extension(oauth_client))
+        .layer(Extension(pool.clone()));
+    let user_routes = Router::new()
+        .route("/me", get(user_info_handler))
+        .route("/logout", get(logout_handler))
+        .route_layer(RequireAuthorizationLayer::<i64, User>::login());
+    let api_routes = Router::new()
+        .nest("/buckets", bucket_routes)
+        .nest("/auth", auth_routes)
+        .nest("/user", user_routes);
 
-    let app = Router::new().nest("/api", api_routes).layer(middleware);
+    let shared_state = AppState { pool };
+
+    let app = Router::new()
+        .nest("/api", api_routes)
+        .layer(middleware)
+        .layer(auth_layer)
+        .layer(session_layer)
+        .with_state(shared_state);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("listening on {}", addr);
@@ -146,8 +245,8 @@ async fn delete_request(
     }
     Ok(StatusCode::NO_CONTENT)
 }
-// Handler that streams the request body to a file.
-async fn save_request_body(
+
+async fn save_request(
     Path(params): Path<(String, String)>,
     body: BodyStream,
 ) -> Result<(), (StatusCode, String)> {
@@ -241,16 +340,164 @@ fn is_file(path: &str) -> bool {
     let split: Vec<&str> = path.split(".").collect();
     split.len() > 1
 }
-//
-// fn path_is_valid(path: &str) -> bool {
-//     let path = std::path::Path::new(path);
-//     let mut components = path.components().peekable();
-//
-//     if let Some(first) = components.peek() {
-//         if !matches!(first, std::path::Component::Normal(_)) {
-//             return false;
-//         }
-//     }
-//
-//     components.count() == 1
-// }
+
+fn build_discord_oauth_client() -> BasicClient {
+    let client_id = env::var("CLIENT_ID").expect("Missing CLIENT_ID!");
+    let client_secret = env::var("CLIENT_SECRET").expect("Missing CLIENT_SECRET!");
+    let redirect_url = "http://localhost:3000/api/auth/discord/callback".to_string();
+
+    let auth_url =
+        AuthUrl::new("https://discord.com/api/oauth2/authorize?response_type=code".to_string())
+            .expect("Invalid authorization endpoint URL");
+    let token_url = TokenUrl::new("https://discord.com/api/oauth2/token".to_string())
+        .expect("Invalid token endpoint URL");
+
+    BasicClient::new(
+        ClientId::new(client_id),
+        Some(ClientSecret::new(client_secret)),
+        auth_url,
+        Some(token_url),
+    )
+    .set_redirect_uri(RedirectUrl::new(redirect_url).unwrap())
+}
+async fn login_handler(
+    Extension(client): Extension<BasicClient>,
+    mut session: WritableSession,
+) -> impl IntoResponse {
+    // Generate the authorization URL to which we'll redirect the user.
+    let (auth_url, csrf_state) = client
+        .authorize_url(CsrfToken::new_random)
+        .add_scope(Scope::new("identify".to_string()))
+        .add_scope(Scope::new("email".to_string()))
+        .url();
+
+    // Store the csrf_state in the session so we can assert equality in the callback
+    session.insert("csrf_state", csrf_state).unwrap();
+
+    // Redirect to your oauth service
+    Redirect::to(auth_url.as_ref())
+}
+
+async fn logout_handler(mut auth: AuthContext) -> impl IntoResponse {
+    dbg!("Logging out user: {}", &auth.current_user);
+    auth.logout().await
+}
+#[derive(Debug, Deserialize)]
+struct AuthRequest {
+    code: String,
+    state: CsrfToken,
+}
+#[derive(Debug, Serialize, Deserialize)]
+struct DiscordUser {
+    id: String,
+    avatar: Option<String>,
+    username: String,
+    email: Option<String>,
+    discriminator: String,
+}
+async fn oauth_callback_handler(
+    mut auth: AuthContext,
+    Query(query): Query<AuthRequest>,
+    Extension(pool): Extension<SqlitePool>,
+    Extension(oauth_client): Extension<BasicClient>,
+    session: ReadableSession,
+) -> impl IntoResponse {
+    log::debug!("Running oauth callback {query:?}");
+    // Compare the csrf state in the callback with the state generated before the
+    // request
+    let original_csrf_state: CsrfToken = session.get("csrf_state").unwrap();
+    let query_csrf_state = query.state.secret();
+    let csrf_state_equal = original_csrf_state.secret() == query_csrf_state;
+
+    drop(session);
+
+    if !csrf_state_equal {
+        log::debug!("csrf state is invalid, cannot login",);
+
+        // Return to some error
+        return Redirect::to("/protected");
+    }
+
+    log::debug!("Getting oauth token");
+    // Get an auth token
+    let token = oauth_client
+        .exchange_code(AuthorizationCode::new(query.code))
+        .request_async(async_http_client)
+        .await
+        .unwrap();
+
+    // Fetch user data from discord
+    let client = reqwest::Client::new();
+    let user_data = client
+        // https://discord.com/developers/docs/resources/user#get-current-user
+        .get("https://discordapp.com/api/users/@me")
+        .bearer_auth(token.access_token().secret())
+        .send()
+        .await
+        .unwrap()
+        .json::<DiscordUser>()
+        .await
+        .unwrap();
+    log::debug!("Getting db connection");
+
+    let Some(email) = user_data.email else {
+        return Redirect::to("/no-email");
+    };
+
+    // Fetch the user and log them in
+    let mut conn = pool.acquire().await.unwrap();
+    log::debug!("Getting user");
+    let user: Option<User> = sqlx::query_as!(User, "select * from users where email = $1", email)
+        .fetch_optional(&mut conn)
+        .await
+        .unwrap();
+    let user = match user {
+        Some(user) => user,
+        None => {
+            // let Some(_invite) = sqlx::query_as!(
+            //     UserInvite,
+            //     "select * from user_invites where email = $1",
+            //     email
+            // )
+            // .fetch_optional(&mut conn)
+            // .await
+            // .unwrap() else {
+            //     return Redirect::to("/no-invite");
+            // };
+
+            sqlx::query!(
+                "insert into users (password_hash, name, email) values ($1, $2, $3);",
+                user_data.username,
+                user_data.username,
+                email
+            )
+            .execute(&mut conn)
+            .await
+            .unwrap();
+            let user: User = sqlx::query_as!(User, "select * from users where email = $1", email)
+                .fetch_one(&mut conn)
+                .await
+                .unwrap();
+            user
+        }
+    };
+    log::debug!("Got user {user:?}. Logging in.");
+
+    auth.login(&user).await.unwrap();
+
+    log::debug!("Logged in the user: {user:?}");
+
+    Redirect::to("http://localhost:5173/")
+}
+
+#[derive(Serialize, Deserialize)]
+struct UserInfo {
+    name: String,
+    email: String,
+}
+async fn user_info_handler(Extension(user): Extension<User>) -> impl IntoResponse {
+    Json(UserInfo {
+        name: user.name,
+        email: user.email,
+    })
+}
